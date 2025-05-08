7b9d8805c2d9edbdccde183b3f90fc1af7dada21
Sebitass47 <smdlr47@hotmail.com>
2025-05-08 09:25:43 -0600

    FIX_GH_pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
 import Nosotros from '../pages/Nosotros.vue';
 import Contact from '../pages/Contact.vue';
@@ -12,7 +12,7 @@ const routes = [
 ];  
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
